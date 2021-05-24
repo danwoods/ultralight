48f82579976230883b3197c1352f30045b871608
Dan Woodson <woodson.dan@gmail.com>
2021-05-23 22:38:00 -0600

    Style update

--- util/logger.ts
+++ util/logger.ts
@@ -6,8 +6,8 @@
  * @returns {Object} Wrapper for console
  */
 export const logger = (name: string) => ({
-  info: (msg: string, ...rest) => console.info(`[[${name}]] ${msg}`, rest),
-  error: (msg: string, ...rest) => console.error(`[[${name}]] ${msg}`, rest),
-  debug: (msg: string, ...rest) => console.debug(`[[${name}]] ${msg}`, rest),
-  warn: (msg: string, ...rest) => console.warn(`[[${name}]] ${msg}`, rest)
+  info: (msg: string, ...rest) => console.info(`[[ ${name} ]] ${msg}`, rest),
+  error: (msg: string, ...rest) => console.error(`[[ ${name} ]] ${msg}`, rest),
+  debug: (msg: string, ...rest) => console.debug(`[[ ${name} ]] ${msg}`, rest),
+  warn: (msg: string, ...rest) => console.warn(`[[ ${name} ]] ${msg}`, rest)
 })
