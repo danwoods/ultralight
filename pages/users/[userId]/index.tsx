3674bacb928436c3ee9f17334c281d0fda0c8fe1
Dan Woodson <woodson.dan@gmail.com>
2021-12-24 12:57:50 -0700

    Updated main page to be be user instead of projects

--- pages/users/[userId]/index.tsx
+++ pages/users/[userId]/index.tsx
@@ -1,29 +1,23 @@
 import React from 'react'
 import Link from 'next/link'
-import { useProjects } from '../../../util/data/useProjects'
 import { useAuth } from '../../../util/auth/useAuth'
 import { withRequireAuth } from '../../../util/auth/RequireAuthHOC'
 
-export const Pjct = () => {
+export const User = () => {
   const { userId } = useAuth()
-  const { data: projects } = useProjects(userId)
 
   return (
     <div>
-      {'Projects'}
+      {'User'}
       <ol>
-        {projects.map((p) => (
-          // @ts-ignore
-          <li key={p.id}>
-            {/* @ts-ignore */}
-            <Link href={`/users/${userId}/projects/${p.id}`}>
-              <a>{p.name}</a>
-            </Link>
-          </li>
-        ))}
+        <li>
+          <Link href={`/users/${userId}/projects`}>
+            <a>{'Projects'}</a>
+          </Link>
+        </li>
       </ol>
     </div>
   )
 }
 
-export default withRequireAuth(Pjct)
+export default withRequireAuth(User)
