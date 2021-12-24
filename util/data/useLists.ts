fb552910d810993a0c9bed64d7f8ed544364278e
Dan Woodson <woodson.dan@gmail.com>
2021-12-24 12:42:12 -0700

    Added useListItems #17

--- util/data/useLists.ts
+++ util/data/useLists.ts
@@ -2,16 +2,14 @@
 
 import '../../components/Auth/init'
 import { useCollection } from './useCollection'
-
-type ListData = {
-  id: string
-  name: string
-}
+import type List from '../data/list/type'
+import type { ListItem } from '../data/list/item'
 
 /** Project database */
 
-type ReturnValue = {
-  data: ListData[]
+type UseListsReturnValue = {
+  data: List[]
+  add: (arg0: List) => Promise<List>
 }
 
 /**
@@ -22,6 +20,26 @@ type ReturnValue = {
 export const useLists = (
   userId: string | null | undefined,
   projectId: string | null | undefined
-): ReturnValue => {
-  return useCollection<ListData>(`Users/${userId}/Projects/${projectId}/Lists`)
+): UseListsReturnValue => {
+  return useCollection<List>(`Users/${userId}/Projects/${projectId}/Lists`)
+}
+
+type UseListItemsReturnValue = {
+  data: ListItem[]
+  add: (arg0: ListItem) => Promise<ListItem>
+}
+
+/**
+ * Hook to work with Lists
+ * @param {Object} config Config object for useSWR
+ * @returns {Object} {data: Projects[], add: name => Promise, remove: id => Promise}
+ */
+export const useListItems = (
+  userId: string | null | undefined,
+  projectId: string | null | undefined,
+  listId: string | null | undefined
+): UseListItemsReturnValue => {
+  return useCollection<ListItem>(
+    `Users/${userId}/Projects/${projectId}/Lists/${listId}/Items`
+  )
 }
