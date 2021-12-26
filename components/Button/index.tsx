2fcc681b397e5d8b484ed2305f3984726ef43733
Dan Woodson <woodson.dan@gmail.com>
2021-12-26 13:56:57 -0700

    Added new tailwind class resolution utility and implemented in Button component

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,16 +1,28 @@
 /** @file Default Buttion */
 import React from 'react'
+import { classNames } from '../../util/styling/classNames'
+
+const classes = {
+  root: `
+    m-8
+    ml-auto
+    p-5
+    bg-primary
+    hover:bg-primary-dark
+    disabled:bg-primary-light
+    text-white
+    font-bold
+    py-2
+    rounded
+    cursor-pointer
+  `
+}
 
 /**
  * Default Button
  */
 export const Button = (
   props: React.HTMLAttributes<HTMLButtonElement> & { disabled?: boolean }
-) => (
-  <button
-    {...props}
-    className="m-8 ml-auto p-5 bg-primary hover:bg-primary-dark disabled:bg-primary-light text-white font-bold py-2 rounded cursor-pointer"
-  />
-)
+) => <button {...props} className={classNames(classes.root, props.className)} />
 
 export default Button
---
+++ util/styling/classNames.ts
@@ -0,0 +1,5 @@
+import classNamesOriginal from 'classnames'
+import { overrideTailwindClasses } from 'tailwind-override'
+
+export const classNames = (...args) =>
+  overrideTailwindClasses(classNamesOriginal(...args))
