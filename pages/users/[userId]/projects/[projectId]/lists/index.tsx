d79245a6140c26f648590bf453423dd567fdfa60
Dan Woodson <woodson.dan@gmail.com>
2021-12-30 09:20:55 -0700

    Added add functionality to lists

--- pages/users/[userId]/projects/[projectId]/lists/index.tsx
+++ pages/users/[userId]/projects/[projectId]/lists/index.tsx
@@ -1,12 +1,41 @@
 /** @file Page for a list of lists */
 
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import { useAuth } from '../../../../../../util/auth/useAuth'
 import { withRequireAuth } from '../../../../../../util/auth/RequireAuthHOC'
 import { useLists } from '../../../../../../util/data/useLists'
 import { useRouter } from 'next/router'
+import { Button } from '../../../../../../components/Button'
 
+type AddItemInputProps = {
+  onAdd: (name: string) => Promise<any>
+}
+
+const AddItemInput = (props: AddItemInputProps) => {
+  const [newItemName, setNewItemName] = useState('')
+
+  const onAddClick = () =>
+    props.onAdd(newItemName).then(() => setNewItemName(''))
+
+  return (
+    <div className="flex items-center rounded-md">
+      <div className="pl-2 flex w-full">
+        <input
+          className="w-full rounded-md bg-gray-200 text-gray-700 leading-tight focus:outline-none py-2 px-2"
+          id="search"
+          onChange={(e) => setNewItemName(e.target.value)}
+          placeholder="Add"
+          type="text"
+          value={newItemName}
+        />
+        <Button className={'p-2 m-0'} onClick={onAddClick}>
+          {'Add'}
+        </Button>
+      </div>
+    </div>
+  )
+}
 /**
  * Page of lists
  * @returns {JSX.Element} Page of lists
@@ -15,7 +44,7 @@ export const Lists = () => {
   const { userId } = useAuth()
   const router = useRouter()
   const { projectId } = router.query
-  const { data: lists } = useLists(userId, String(projectId))
+  const { data: lists, add } = useLists(userId, String(projectId))
 
   return (
     <div>
@@ -29,6 +58,8 @@ export const Lists = () => {
           </li>
         ))}
       </ol>
+      {/* @ts-ignore */}
+      <AddItemInput onAdd={(name: string) => add({ name: name })} />
     </div>
   )
 }
